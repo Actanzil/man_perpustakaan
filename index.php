34ca0cd44ab9a1d3a53a4b0d6f90866c4de73d85
Actanzil <tanziluadjiganesha@gmail.com>
2024-05-24 23:24:02 +0700

    Login Clearfix

--- index.php
+++ index.php
@@ -60,9 +60,9 @@
     <head> 
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <!-- Primary Meta Tags -->
-        <title>Pupuk - Admin</title>
+        <title>Perpustakaan - Admin</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="title" content="Volt - Free Bootstrap 5 Dashboard">
+        <meta name="title" content="Perpustakaan - Administrasi">
         <meta name="author" content="Themesberg">
 
         <!-- Favicon -->
@@ -104,12 +104,12 @@
                             <form action="" method="POST" class="mt-4">
                                 <!-- Form -->
                                 <div class="form-group mb-4">
-                                    <label for="email">Email</label>
+                                    <label for="email">Username</label>
                                     <div class="input-group">
                                         <span class="input-group-text" id="basic-addon1">
                                             <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                                         </span>
-                                        <input type="text" class="form-control" placeholder="example@company.com" id="email" name="username" required>
+                                        <input type="text" class="form-control" placeholder="Username" name="username" required>
                                     </div>  
                                 </div>
                                 <!-- End of Form -->
